0441807ae0e0327f46d154e5c7843f2f60a1e509
Jennifer Powell <jerpowel321@gmail.com>
2020-04-02 15:13:00 -0700

    update router.js file

--- router.js
+++ router.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import Portfolio from "./pages/portfolio";
 import About from "./pages/about"
+import { useRouterHistory, Router } from 'react-router'
+import { createHistory } from 'history'
 
-export default <Router >
+const history = useRouterHistory(createHistory)({
+    basename: '/test'
+  })
+
+export default <Router history={history} >
   <Route path="/" component={App}>
     <IndexRoute component={Portfolio}/>
     <Route path="demo" component={About}/>
