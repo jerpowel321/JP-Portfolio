ac0c0d090b343def96590f2089896aad2c7f93dc
Jennifer Powell <jerpowel321@gmail.com>
2020-04-02 23:06:40 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-  
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route} from "react-router-dom";
-import Portfolio from "./pages/portfolio";
-import About from "./pages/about"
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
+import React, { Component } from 'react'
 
-function App() {
-  const theme = createMuiTheme({
-    palette: {
-      primary: purple,
-      secondary: green,
-    },
-  });
-  return (
-    <ThemeProvider theme={theme}>
-    <Router>
-    <Route exact path="/" component={Portfolio}/>
-    <Route exact path="/about" component={About}/>
-    </Router>
-    </ThemeProvider>
-  );
+class App extends Component {
+static propTypes = {
+  children: PropTypes.node
 }
 
-export default App;
+render() {
+  const { children } = this.props
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+}
+
+export default App
